0bfb4f36e75bbc7e0cb53827651cf9bc767e00da
Punyaphat Kaewkao <punyaphat0010@gmail.com>
2025-09-01 10:33:53 +0700

    comment server for test

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const mysql = require("mysql");
+// const express = require("express");
+// const mysql = require("mysql");
 
-const app = express();
-const port = 4000;
+// const app = express();
+// const port = 4000;
 
-// MySQL database connection configuration
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "username", // แทนที่ด้วยชื่อผู้ใช้ MySQL ของคุณ
-  password: "password", // แทนที่ด้วยรหัสผ่าน MySQL ของคุณ
-  database: "database_name" // แทนที่ด้วยชื่อฐานข้อมูลที่คุณต้องการเชื่อมต่อ
-});
+// // MySQL database connection configuration
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "username", // แทนที่ด้วยชื่อผู้ใช้ MySQL ของคุณ
+//   password: "password", // แทนที่ด้วยรหัสผ่าน MySQL ของคุณ
+//   database: "database_name" // แทนที่ด้วยชื่อฐานข้อมูลที่คุณต้องการเชื่อมต่อ
+// });
 
-// Connect to MySQL database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to MySQL database as id ' + connection.threadId);
-});
+// // Connect to MySQL database
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL database: ' + err.stack);
+//     return;
+//   }
+//   console.log('Connected to MySQL database as id ' + connection.threadId);
+// });
 
-// Middleware to log request method and URL
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+// // Middleware to log request method and URL
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// });
 
-// Route handling
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// // Route handling
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.get("/about", (req, res) => {
-  res.send("About page");
-});
+// app.get("/about", (req, res) => {
+//   res.send("About page");
+// });
 
